16cf754564a958d85fab0846785ebc9a4a946b20
easalgado <202100446@umh.edu.hn>
2022-03-17 15:23:45 -0600

    avance routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppComponent } from './app.component';
 import { DescripcionComponent } from './pages/descripcion/descripcion.component';
 import { LugarNacimientoComponent } from './pages/lugar-nacimiento/lugar-nacimiento.component';
 import { MetasComponent } from './pages/metas/metas.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { MetasComponent } from './pages/metas/metas.component';
     MetasComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
