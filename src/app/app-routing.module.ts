8f08d31538f03d4e21459b16608754a1cb6400b7
easalgado <202100446@umh.edu.hn>
2022-03-17 15:23:16 -0600

    Avance Routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { LugarNacimientoComponent } from './pages/lugar-nacimiento/lugar-nacimiento.component';
+import { DescripcionComponent } from './pages/descripcion/descripcion.component';
+import { MetasComponent } from './pages/metas/metas.component';
+
+const routes: Routes =[
+  {
+    path: 'descripcion',
+    component: DescripcionComponent
+    },
+    {
+    path: 'lugar-nacimiento',
+    component: LugarNacimientoComponent
+    },
+    {
+    path: 'metas',
+    component: MetasComponent
+    },
+    {
+      path: '**',
+      redirectTo: 'descripcion'
+      }
+     
+   ];
+   
+
+
+@NgModule({
+  declarations: [],
+  exports:[
+    RouterModule
+  ],
+  imports: [
+    RouterModule.forRoot(routes)
+  ]
+})
+export class AppRoutingModule { }
